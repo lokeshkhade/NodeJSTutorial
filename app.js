aa11fa62f6a8325b5f303b07298ef568d74eaf42
kavitasingh07 <kavitasinghchips@gmail.com>
2023-02-19 15:49:59 +0530

    2. Environment Variables

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(3000, () => {
-    console.log("listening on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
