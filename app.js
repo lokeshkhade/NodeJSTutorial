627e46e04e08563e55ac4af4bdc59534073b814a
kavitasingh07 <kavitasinghchips@gmail.com>
2023-02-19 22:32:26 +0530

    4. Handling HTTP GET Requests

--- app.js
+++ app.js
@@ -2,11 +2,43 @@ const express = require('express'); //Load express moudule which returns a funct
 const app = express(); //express fucntion retuns object of type express,by convention we call the object as app.app object support varios method get,post,put
 
 
+const courses = [
+    { id: 1, course: 'Maths' },
+    { id: 2, course: 'English' },
+    { id: 3, course: 'Hindi' }
+];
+
 // First argument in get fuction is url or path.second parameter is a callback function which is called when we have http request to this endpoint '/'.This req object has bunch of properties ex.re
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(3000, () => {
-    console.log("listening on port 3000");
+
+//To define a parameter we use :id(id is name of parameter)  
+// In order to read params we req.params.id
+app.get('/api/courses/:id', (req, res) => {
+
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course)
+        res.status(404).send('Course Not Found');
+    res.send(course);
+});
+//Multiple parameter
+// localhost:3000/api/courses/2023/1
+app.get('/api/courses/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+//QuerString : To read query params we use req.query
+//localhost:3000/api/courses/2023/1?sortBy=name
+app.get('/api/courses/:year/:month', (req, res) => {
+    res.send(req.query);
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+
 });
