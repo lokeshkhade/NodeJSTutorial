a3c5200b2a8cdff6b3230624759cbf5d428d3647
kavitasingh07 <kavitasinghchips@gmail.com>
2023-02-19 22:08:37 +0530

    3. Route Parameters

--- app.js
+++ app.js
@@ -7,6 +7,29 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
+<<<<<<< Updated upstream
 app.listen(3000, () => {
     console.log("listening on port 3000");
+=======
+//To define a parameter we use :id(id is name of parameter)  
+// In order to read params we req.params.id
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+//Multiple parameter
+// localhost:3000/api/courses/2023/1
+app.get('/api/courses/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+//QuerString : To read query params we use req.query
+//localhost:3000/api/courses/2023/1?sortBy=name
+app.get('/api/courses/:year/:month', (req, res) => {
+    res.send(req.query);
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+>>>>>>> Stashed changes
 });
