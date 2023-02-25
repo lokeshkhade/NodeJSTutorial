c41146b76ebe68c1e925381f8dfef0700bf871ab
lokeshkhade@gmail.com <lokeshkhade@gmail.com>
2023-02-26 00:00:07 +0530

    8. Handling HTTP PUT Requests

--- app.js
+++ app.js
@@ -55,18 +55,6 @@ app.post('/api/courses', (req, res) => {
 });
 
 
-
-
-app.post('/api/courses', (req, res) => {
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
-});
-
-
 app.put('/api/courses/:id', (req, res) => {
     //Find Course 
     const course = courses.find(c => c.id === parseInt(req.params.id));
